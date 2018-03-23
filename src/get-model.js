a874ee78e7b761cccef8d5f3e0887c8de8ec8854
vanya andreychuk <vandreychyk@gmail.com>
2018-03-23 17:28:31 +0200

    Feat: Add db

--- src/get-model.js
+++ src/get-model.js
@@ -3,16 +3,20 @@ var getPostfix = require('./get-postfix');
 var createConnection = require('./create-connection');
 
 module.exports = function (instance) {
-  return function (name, postfixData, postfixPath) {
-    if (!instance) instance = singleton();
-    var mongoose = instance.mongoose;
-    var schemas = instance.schemas;
-    var url = instance.url;
+    return function (name, postfixData, postfixPath, db) {
+        if (!instance) instance = singleton();
+        var mongoose = instance.mongoose;
+        var schemas = instance.schemas;
+        var url = instance.url;
 
-    var postfix = getPostfix(postfixData, postfixPath);
-    if (postfix) url = url + '-' + postfix;
+        var postfix = getPostfix(postfixData, postfixPath);
+        if (!db && postfix) {
+            url = url + '-' + postfix;
+        } else if (db && postfix) {
+            url = db + '-' + postfix;
+        }
 
-    var connection = createConnection(mongoose, url);
-    return connection.model(name, schemas[name]);
-  }
+        var connection = createConnection(mongoose, url);
+        return connection.model(name, schemas[name]);
+    }
 };
